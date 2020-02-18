70dc90bbad0c1ed80e8b5f9210af2d65efbd1869
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2020-02-17 19:35:41 -0800

    Run this file

--- RobotsGraphics/SimpleDraw.java
+++ RobotsGraphics/SimpleDraw.java
@@ -2,7 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.Graphics2D; 
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
